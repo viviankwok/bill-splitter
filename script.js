96f93ac88590942152cd339d072099ec002f2669
viviankwok <104907963+viviankwok@users.noreply.github.com>
2022-06-06 18:55:40 +0800

    created generateTable fx
    generates table in summary table div based on num of pax and items entered

--- script.js
+++ script.js
@@ -1,16 +1,45 @@
+// runs when generate-table-btn is clicked
 const generateTable = (e) => {
   e.preventDefault();
-  const numRows = document.querySelector("#num-food-inputBox").value;
-  const numCols = document.querySelector("#num-pax-inputBox").value;
+
+  // assigns row and column values
+  let numRows = document.querySelector("#num-food-inputBox").value;
+  let numCols = document.querySelector("#num-pax-inputBox").value;
   if (numCols === "" || numRows === "") {
     alert("Pls fill in both fields for me to pass my project :) Tyvm");
   } else if (numCols <= 0 || numRows <= 0) {
     alert("Pls fill a valid number for me to pass my project :) Tyvm");
   }
+  numRows = parseInt(numRows) + 1;
+  numCols = parseInt(numCols) + 2;
   console.log(`numRows: ${numRows}, numCols: ${numCols}`);
+
+  // create rows: <tr>
+  let tableTemplate = "";
+  for (let i = 1; i <= numRows; i++) {
+    tableTemplate += "<tr>";
+    // create columns
+    for (let i = 1; i <= numCols; i++) {
+      if (i === 1) {
+        tableTemplate +=
+          '<td><input type="text" placeholder="food item"></input></td>';
+      } else if (i === 2) {
+        tableTemplate +=
+          '<td><input type="text" placeholder="item price"></input></td>';
+      } else {
+        tableTemplate +=
+          '<td><input type="text" placeholder="testing input"></input></td>';
+      }
+    }
+    tableTemplate += "</tr>";
+  }
+  //   console.log(tableTemplate);
+
+  const testingTable = document.querySelector("#testing-table");
+  testingTable.innerHTML = tableTemplate;
 };
 
-// when 'generate table' button is clicked, run generateTable function
+// listens for click on generate-table-btn
 document
   .querySelector("#generate-table-btn")
   .addEventListener("click", generateTable);
